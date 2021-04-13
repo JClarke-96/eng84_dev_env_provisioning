c9e47c5275b6928cecd4d68b3058d10ec5f8e1a5
Ben <benranson5@gmail.com>
2021-04-13 17:06:25 +0100

    created a multimachine enviroment with mongodb

--- project/environment/provision_app.sh
+++ project/environment/provision_app.sh
@@ -11,9 +11,8 @@ sudo apt-get install nginx -y
 sudo apt-get install npm -y
 
 # node js
-curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
-sudo sh -c "echo deb https://deb.nodesource.com/node_6.x xenial main \> /etc/apt/sources.list.d/nodesource.list" 
-sudo apt-get update -y
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
 # pm2
